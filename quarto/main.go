b27390419c4e56fa58acf19c613589d6369e7faf
Brian Clymer <crdnilfan@gmail.com>
2013-08-25 00:03:48 -0500

    Get stuff to actually compile again.

--- quarto/main.go
+++ quarto/main.go
@@ -6,6 +6,7 @@ import (
     "io/ioutil"
     "code.google.com/p/go.net/websocket"
     "encoding/json"
+    "quarto/realtime"
 )
 
 type Page struct {
@@ -17,22 +18,14 @@ type MessageToWhom struct {
 	ToUuid	string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    title := "Quarto Online!"
-    p := loadPage(title)
-    t, _ := template.ParseFiles("index.html")
-    w.Header().Set("Content-Type", "text/html")
-    t.Execute(w, p)
-}
-
 func realtimeHost(ws *websocket.Conn) {
 	uuid := ws.Request().URL.Query().Get("uuid")
-	subscription := Subscribe(uuid)
+	subscription := realtime.Subscribe(uuid)
 	subscription.Uuid = uuid;
 	defer subscription.Cancel()
 
-	Join(uuid)
-	defer Leave(uuid)
+	realtime.Join(uuid)
+	defer realtime.Leave(uuid)
 
 	newMessages := make(chan string)
 	go func() {
@@ -64,18 +57,26 @@ func realtimeHost(ws *websocket.Conn) {
 			b := []byte(msg)
 			json.Unmarshal(b, &m)
 
-			Action("action", msg, uuid, m.ToUuid)
+			realtime.Action("action", msg, uuid, m.ToUuid)
 		}
 	}
 
 	return
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    title := "Quarto Online!"
+    p := loadPage(title)
+    t, _ := template.ParseFiles("../index.html")
+    w.Header().Set("Content-Type", "text/html")
+    t.Execute(w, p)
+}
+
 func main() {
     http.HandleFunc("/", handler)
     http.Handle("/realtime", websocket.Handler(realtimeHost))
-    http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
-    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+    http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("../js"))))
+    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("../css"))))
     http.ListenAndServe(":8080", nil)
 }
 
