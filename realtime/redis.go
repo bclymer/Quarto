23196644f020582dacae2ef282e37f68f4dbd0f6
Brian Clymer <crdnilfan@gmail.com>
2013-09-26 00:30:33 -0500

    Now redis auth.

--- realtime/redis.go
+++ realtime/redis.go
@@ -1,6 +1,8 @@
 package realtime
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"menteslibres.net/gosexy/redis"
 )
@@ -13,11 +15,28 @@ var (
 	client *redis.Client
 )
 
+type RedisAuth struct {
+	Password string `json:"password"`
+}
+
 func ConnectRedis() *redis.Client {
+	var redisAuth RedisAuth
+	content, err := ioutil.ReadFile("../realtime/redisAuth.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(content, &redisAuth)
+	if err != nil {
+		panic(err)
+	}
 	client = redis.New()
-	err := client.Connect("bclymer.com", 6379)
+	err = client.Connect("bclymer.com", 6379)
 	if err != nil {
 		log.Fatalln("Connect to Redis:", err)
+	}
+	_, err = client.Auth(redisAuth.Password)
+	if err != nil {
+		panic(err)
 	}
 	return client
 }
